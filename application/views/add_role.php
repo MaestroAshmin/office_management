d038e8f6854531a07e6ecc1712506d4982e05d7e
Raghbendra Shrestha <ra9havsh@gmail.com>
2020-06-15 17:22:18 +0545

    view user type

--- application/views/add_role.php
+++ application/views/add_role.php
@@ -7,42 +7,33 @@
 
     <!-- Content Row -->
     <div class="row">
-         <div class="col-sm-12 col-md-7">
+         <div class="col-sm-12 col-md-5">
             <table class="table table-bordered">
                 <thead class="thead-dark">
                     <tr>
+                        <td colspan="2">
+                            <form action="<?php echo site_url();?>user/add_role" method="post" enctype="multipart/form-data">
+                                <input type="text" name="user_type" class="form-control col-sm-7 float-left">                              
+                                    <button class="btn btn-primary" style="margin-left:10px;">Add</button>            
+                            </form>
+                        </td>
+                    </tr>    
+                    <tr>
                         <th scope="col">User Type</th>
-                        <th scope="col">Action</th>
+                        <th scope="col" class="text-center">Action</th>
                     </tr>
                 </thead>
                 <tbody>
                 <?php foreach ($roles as $role){ ?>
                     <tr>
                         <td><?php echo $role['user_type']?></td>
-                        <td>
+                        <td class="text-center">
                             <a href="<?php echo site_url('user/delete_role/'.$role['role_id']) ?>" class="delete btn btn-danger" data-confirm="Are you sure to delete this item?" style="display:inline-block">Delete</a>
                         </td>
                     </tr>
                     <?php } ?>
-                    <tr>
-                        <td colspan="2">
-                            <form action="<?php echo site_url();?>user/add_role" method="post" enctype="multipart/form-data">
-                                <div class="form-group">
-                                    <label>User Type</label>
-                                    <input type="text" name="user_type" class="form-control">
-                                </div>
-                                <div class="form-group float-left">
-                                    <button class="btn btn-primary float-left">Add</button>
-                                </div>
-                            </form>
-                        </td>
-                    </tr>    
                 </tbody>
             </table>       
-        </div>
-        <div class="col-sm-12 col-md-5 text-center">
-           
-        <!-- Content Row -->
         </div>
     </div>
 </div>
