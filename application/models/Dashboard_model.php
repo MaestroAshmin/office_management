c6a664911a019eb326c17644e78c0c6eb40128ac
Raghbendra Shrestha <ra9havsh@gmail.com>
2020-06-21 22:19:02 +0545

    Dashboard_model

--- application/models/Dashboard_model.php
+++ application/models/Dashboard_model.php
@@ -28,6 +28,82 @@ class Dashboard_model extends CI_Model{
 		}
 	}
 
+	public function get_monthly_income($year){
+		$result= $this->db->select("SUM(amount) as amount, nepali_date as date")->from('tbl_income_data')
+                    -> where("nepali_date like '".$year."%'")
+                    -> group_by('nepali_date')->get()
+       				-> result_array();
+       	if(!empty($result)){
+       		return $result;
+       	}else{
+       		return false;
+        }
+	}
+	
+	public function get_current_year_expense($year){
+		$result= $this->db->select("SUM(amount) as amount")->from('tbl_expense_data')
+                    -> where("nepali_date like '".$year."%'")
+                    ->get()
+       				-> result_array();
+       	if(!empty($result)){
+       		return $result;
+       	}else{
+       		return false;
+        }
+    }
+    public function get_current_year_income($year){
+		$result= $this->db->select("SUM(amount) as amount")->from('tbl_income_data')
+                    -> where("nepali_date like '".$year."%'")
+                    ->get()
+       				-> result_array();
+       	if(!empty($result)){
+       		return $result;
+       	}else{
+       		return false;
+		}
+    }
+	public function get_total_year_expense(){
+		$result= $this->db->select("SUM(amount) as amount")->from('tbl_expense_data')
+                    ->get()
+       				-> result_array();
+       	if(!empty($result)){
+       		return $result;
+       	}else{
+       		return false;
+        }
+	}
+	
+    public function get_total_year_income(){
+		$result= $this->db->select("SUM(amount) as amount")->from('tbl_income_data')
+                    ->get()
+       				-> result_array();
+       	if(!empty($result)){
+       		return $result;
+       	}else{
+       		return false;
+		}
+	}
+    public function get_yearly_income(){
+		$result= $this->db->select("SUM(amount) as amount, nepali_date as date")->from('tbl_income_data')
+                    -> group_by('nepali_date')->get()
+       				-> result_array();
+       	if(!empty($result)){
+       		return $result;
+       	}else{
+       		return false;
+        }
+    }
+    public function get_yearly_expense(){
+		$result= $this->db->select("SUM(amount) as amount, nepali_date as date")->from('tbl_expense_data')
+                    -> group_by('nepali_date')->get()
+       				-> result_array();
+       	if(!empty($result)){
+       		return $result;
+       	}else{
+       		return false;
+        }
+	}
+
 	public function get_live($date, $user_id)
 	{
 		if($user_id == 1){
@@ -110,80 +186,6 @@ class Dashboard_model extends CI_Model{
 		else{
 			return false;
 		}
-	}
-    public function get_monthly_income($year){
-		$result= $this->db->select("SUM(amount) as amount, nepali_date as date")->from('tbl_income_data')
-                    -> where("nepali_date like '".$year."%'")
-                    -> group_by('nepali_date')->get()
-       				-> result_array();
-       	if(!empty($result)){
-       		return $result;
-       	}else{
-       		return false;
-        }
-	}
-	
-	public function get_current_year_expense($year){
-		$result= $this->db->select("SUM(amount) as amount")->from('tbl_expense_data')
-                    -> where("nepali_date like '".$year."%'")
-                    ->get()
-       				-> result_array();
-       	if(!empty($result)){
-       		return $result;
-       	}else{
-       		return false;
-        }
-    }
-    public function get_current_year_income($year){
-		$result= $this->db->select("SUM(amount) as amount")->from('tbl_income_data')
-                    -> where("nepali_date like '".$year."%'")
-                    ->get()
-       				-> result_array();
-       	if(!empty($result)){
-       		return $result;
-       	}else{
-       		return false;
-		}
-    }
-	public function get_total_year_expense(){
-		$result= $this->db->select("SUM(amount) as amount")->from('tbl_expense_data')
-                    ->get()
-       				-> result_array();
-       	if(!empty($result)){
-       		return $result;
-       	}else{
-       		return false;
-        }
-    }
-    public function get_total_year_income(){
-		$result= $this->db->select("SUM(amount) as amount")->from('tbl_income_data')
-                    ->get()
-       				-> result_array();
-       	if(!empty($result)){
-       		return $result;
-       	}else{
-       		return false;
-		}
-	}
-    public function get_yearly_income(){
-		$result= $this->db->select("SUM(amount) as amount, nepali_date as date")->from('tbl_income_data')
-                    -> group_by('nepali_date')->get()
-       				-> result_array();
-       	if(!empty($result)){
-       		return $result;
-       	}else{
-       		return false;
-        }
-    }
-    public function get_yearly_expense(){
-		$result= $this->db->select("SUM(amount) as amount, nepali_date as date")->from('tbl_expense_data')
-                    -> group_by('nepali_date')->get()
-       				-> result_array();
-       	if(!empty($result)){
-       		return $result;
-       	}else{
-       		return false;
-        }
 	}
 }
 
