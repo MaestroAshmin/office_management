7721e95bb2d034379dd8eff6f98a9e8947f922b9
Raghbendra Shrestha <ra9havsh@gmail.com>
2020-06-21 10:43:43 +0545

    p

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -60,6 +60,10 @@ class User extends CI_Controller
 		$user_id   = $sess_data['user_id'];
 		$user_role = $sess_data['user_role'];
 
+		// $res 
+		// var_dump($res);exit;
+		// $res 		= $this->user_model->get_monthly_expense(date("Y"));
+
 
 		$data = array(
 			'title' 		=> 'User Dashbaord',
@@ -649,14 +653,14 @@ class User extends CI_Controller
 		$sess_data = $this->session->all_userdata();
 		$user_id   = $sess_data['user_id'];
 		$user_role = $sess_data['user_role'];
-		$is_head = $this->user_model->is_head($user_id);
-		$dept = $this->user_model->find_dept($user_id);
-		$contacts = $this->user_model->view_contacts();
+		// $is_head = $this->user_model->is_head($user_id);
+		// $dept = $this->user_model->find_dept($user_id);
+		// $activity = $this->user_model->view_activity($user_id,$is_head,$dept);
 		$data = array(
 			'title' 		=> 'View Contact Management',
 			'main_content'	=> 'view_contact_management',
 			'role' 			=> $user_role,
-			'contacts'		=> $contacts
+			// 'activity'		=> $activity,
 		);
 		$this->load->view('includes/template', $data);
 	}
@@ -767,43 +771,7 @@ class User extends CI_Controller
 			'role' 			=> $user_role,
 			'target'		=> $target,
 		);
-		$this->load->view('includes/template', $data);
-	}
-	public function get_each_contact(){
-		if($this->session->userdata('user_logged_in') != '1'){
-			redirect('user', 'refresh');
-		}
-		$post = $this->uri->segment(3);
-		$sess_data = $this->session->all_userdata();
-		$user_id   = $sess_data['user_id'];
-		$user_role = $sess_data['user_role'];
-		$contact = $this->user_model->get_each_contact($post);
-		$follow_ups = $this->user_model->get_follow_ups($post);
-
-		$data = array(
-			'title' 		=> 'View Contact',
-			'main_content'	=> 'view_each_contact',
-			'role' 			=> $user_role,
-			'contact'		=> $contact,
-			'follow_ups'	=> $follow_ups
-		);
-		$this->load->view('includes/template', $data);
-	}
-	public function report_generate(){
-		if($this->session->userdata('user_logged_in') != '1'){
-			redirect('user', 'refresh');
-		}
-		$sess_data = $this->session->all_userdata();
-		$user_id   = $sess_data['user_id'];
-		$user_role = $sess_data['user_role'];
-		$reports = $this->user_model->generate_report();
-		$data = array(
-			'title' 		=> 'View Reports',
-			'main_content'	=> 'report_generate',
-			'role' 			=> $user_role,
-			'reports'		=> $reports
-		);
-		// echo '<pre>';print_r($data);exit;
+		// echo '<pre>';print_r($target);exit;
 		$this->load->view('includes/template', $data);
 	}
 	public function logout(){
@@ -923,7 +891,6 @@ class User extends CI_Controller
 		$view = $this->load->view('partials-request-fuel-info', $request_info, true);
 		echo $view;exit;
 	}
-
 
 	public function generate_report(){
 
